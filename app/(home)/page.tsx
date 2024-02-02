4c963ad246d465cb96727bceaf65f3c07cb8e914
MichelleCordeiro <mic321@gmail.com>
2024-02-01 21:20:46 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,19 @@
+import { format } from 'date-fns'
 import Header from '../_components/header';
+import { ptBR } from 'date-fns/locale';
 
 export default function Home() {
   return (
     <div>
       <Header />
-    </div>      
-  )}
+
+      <div className='px-5 pt-5'>
+        <h2 className='text-xl font-bold'>Olá, Bia!</h2>
+        <p className='first-letter:uppercase second-letter:uppercase text-sm'>
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+      </p>
+      </div>
+    </div>
+  );}
