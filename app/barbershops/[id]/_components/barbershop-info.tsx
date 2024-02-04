1d32c12eb79c98724145ac2e7fa2f80c96b38834
MichelleCordeiro <mic321@gmail.com>
2024-02-04 18:03:36 -0300

    feat: add SideMenu component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-import { Button } from '@/app/_components/ui/button';
-import { Barbershop } from '@prisma/client';
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { Barbershop } from '@prisma/client';
+import { Button } from '@/app/_components/ui/button';
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react';
+import { Sheet, SheetTrigger, SheetContent } from '@/app/_components/ui/sheet';
+import SideMenu from '@/app/_components/side-menu';
+import Image from 'next/image';
 
 interface BarbershopInfoProps {
   barbershop: Barbershop
@@ -20,18 +22,28 @@ const BarbarshopInfo = ({ barbershop }: BarbershopInfoProps) => {
   return (
     <div>
       <div className='h-[250px] w-full relative'>
-        <Button
-          onClick={handleBackClick}
-          size='icon'
-          variant='outline'
-          className='z-50 absolute top-4 left-4'
-        >
+        <Button onClick={handleBackClick} size='icon' variant='outline' className='z-50 absolute top-4 left-4' >
           <ChevronLeftIcon />
         </Button>
 
-        <Button size='icon' variant='outline' className='z-50 absolute top-4 right-4'>
+        {/* <Button size='icon' variant='outline' className='z-50 absolute top-4 right-4'>
           <MenuIcon />
-        </Button>
+        </Button> */}
+
+        <Sheet>
+          <SheetTrigger asChild>
+            {/* <Button variant='outline' size='icon' className='h-8 w-8'>
+              <MenuIcon size={16} />
+            </Button> */}
+            <Button size='icon' variant='outline' className='z-50 absolute top-4 right-4'>
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className='p-0'>
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
 
         <Image
           src={barbershop.imageUrl}
