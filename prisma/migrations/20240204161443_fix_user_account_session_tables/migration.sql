ab06787a709334074f197613e8f110193eeab808
MichelleCordeiro <mic321@gmail.com>
2024-02-04 13:34:10 -0300

    fix: tables in schema.prisma

---
+++ prisma/migrations/20240204161443_fix_user_account_session_tables/migration.sql
@@ -0,0 +1,46 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `accessTokenExpires` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `idToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `refreshToken` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `sessionState` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `tokenType` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `status` on the `Booking` table. All the data in the column will be lost.
+  - You are about to drop the `VerificationRequest` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "accessToken",
+DROP COLUMN "accessTokenExpires",
+DROP COLUMN "idToken",
+DROP COLUMN "refreshToken",
+DROP COLUMN "sessionState",
+DROP COLUMN "tokenType",
+ADD COLUMN     "access_token" TEXT,
+ADD COLUMN     "expires_at" INTEGER,
+ADD COLUMN     "id_token" TEXT,
+ADD COLUMN     "refresh_token" TEXT,
+ADD COLUMN     "session_state" TEXT,
+ADD COLUMN     "token_type" TEXT;
+
+-- AlterTable
+ALTER TABLE "Booking" DROP COLUMN "status",
+ALTER COLUMN "date" SET DATA TYPE TIMESTAMP(3);
+
+-- DropTable
+DROP TABLE "VerificationRequest";
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
