8090ab2cf34ea5992abbdd8c4317910182166998
MichelleCordeiro <mic321@gmail.com>
2024-02-04 21:01:54 -0300

    feat: ensure unauthenticated user is redirected to login on booking click

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -16,7 +16,7 @@ const BarbarshopInfo = ({ barbershop }: BarbershopInfoProps) => {
   const router = useRouter()
   
   const handleBackClick = () => {
-    router.back()
+    router.replace('/');
   }
   
   return (
